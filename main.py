9c2c2a1fbf05fca55f37bf86d0847552fe71d578
Asem Shaath <ashaath@montgomerycollege.edu>
2025-01-19 14:20:04 -0500

    steup gitignore

--- main.py
+++ main.py
@@ -1,21 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
 
 driver = webdriver.Chrome()
 
-driver.get("https://www.selenium.dev/selenium/web/web-form.html")
+driver.get("https://www.instagram.com/accounts/login/?next=%2Flogin%2F&source=desktop_nav")
 
 title = driver.title
+print(title)
 
-driver.implicitly_wait(0.5)
-
-text_box = driver.find_element(by=By.NAME, value="my-text")
-submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
-
-text_box.send_keys("Selenium")
-submit_button.click()
-
-message = driver.find_element(by=By.ID, value="message")
-text = message.text
-
-driver.quit()
+time.sleep(190)
